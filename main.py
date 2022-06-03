14c55008cf5524e27e0db33e8c50e98a20684e1f
quellen-sol <marquelle.nesbitt@gmail.com>
2022-06-03 09:01:10 -0700

    Bot timing change

--- main.py
+++ main.py
@@ -57,14 +57,16 @@ class TheReferee(Bot):
 
     def determineNextMidnight(self):
         dt = date.today()
-        midnight = datetime.combine(dt + timedelta(1), time(12,0,0), timezone(timedelta(hours=timezoneOffset)))
+        midnight = datetime.combine(dt, time(12,0,0), timezone(timedelta(hours=timezoneOffset)))
         print(f"Reset time: {midnight}")
+        while datetime.now(timezone(timedelta(hours=timezoneOffset))) > midnight:
+            midnight += timedelta(days=1)
         return midnight
         # return datetime.now() + timedelta(0,10)
 
     def determineNextFriday(self):
         today = date.today()
-        nextFriday = datetime.combine(today + timedelta(days=(4-today.weekday())%7), time(12,0,0), timezone(timedelta(hours=timezoneOffset)))
+        nextFriday = datetime.combine(today + timedelta(days=(4-today.weekday())%7), time(12,1,0), timezone(timedelta(hours=timezoneOffset)))
         while datetime.now(timezone(timedelta(hours=timezoneOffset))) > nextFriday:
             nextFriday += timedelta(days=7)
         print(f"Next Friday: {nextFriday}")
